161948251c059a59ba9bfc8f566b7b827296f651
David Vadney <qj358927@gmail.com>
2013-10-30 18:30:16 -0400

    adding the header file

---
+++ Header.php
@@ -0,0 +1,20 @@
+<!-- Ideally the header will be at the top of every page that we create in the website
+	with possible exceptions but that can be discussed later -->
+	
+<html> <!-- The beginning of our actual document -->
+
+	<head> 
+		<title> UAlbany ACM</title> <!-- This will be the block of text that
+										 will be displayed in the window or tab header-->
+	</head>
+	
+<body> <!-- this is the code that will actually create 
+			things to be viewed in the browser -->
+			
+		<div id='header' style='background-color:#2E9AFE; margin:-10; overflow:hidden;' >
+			<div id='title' style='color:#fff;margin-left:5; float:left;' ><h2><tt>UAlbany ACM</tt></h2></div>
+			<div><img src='images.jpg' style='margin-top:15;max-width:30px; max-height:30px;float:right;margin-right: 50%;'></div>
+		</div><!-- The block of the header that will be visible-->
+		<div style='clear:both;' ></div>
+
+</body>
