816df39d093d0f1369cdbbda0606ba36cf666091
albanyacm <acm.albany@gmail.com>
2013-10-30 19:18:56 +0000

    Adding the existing php and css files to the github account

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<link rel='stylesheet' type='text/css' href='site.css'> 
+</head>
+<body>
+ <h1> SUNY ALBANY ACM coming soon... </h1>
+ <div class='clear'></div>
+<p> Those who plan on working on the site I would recommend attend the Github workshop<p>
+
+<p><a href='http://www.facebook.com/groups/Albany.ACM/'> Friday (10/18) 2:30pm</a><p>
+<div class='box'></div>
+
+<?php echo "PHP works" ?>
+
+</body>
+
+</html>
