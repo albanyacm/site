a9a56faca5328802dbaeb6749ea311d75ad9ff27
UAhmad7 <uahmad7@hotmail.com>
2014-02-10 23:21:34 -0500

    Create default.php
    New design of the home page

---
+++ default.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to ACM</title>
+<link type="text/css" rel="stylesheet" href="main_styles.css"/>
+<link type="text/css" rel="stylesheet" href="css/home_styles.css"/>
+<link type="image/ico" rel="icon" href="images/acm_logo.png"/>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="inc/jscript.js"></script>
+</head>
+<body>
+
+<?php include_once("inc/header.php"); ?>
+
+<div id="main_container">
+
+<?php include_once("inc/left_nav.php"); ?>
+
+<div id="center_container">
+
+<div>Site is currently under construction...</div>
+<img src="images/logo_live.gif"/>
+
+</div>
+
+</div>
+</body>
+</html>
